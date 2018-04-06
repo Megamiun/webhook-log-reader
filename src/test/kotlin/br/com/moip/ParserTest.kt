0ce6e4d4c42e7a069b0508d71a6f231e85c7db11
Gabryel Monteiro <gabryelarm@gmail.com>
2018-04-06 19:31:07 -0300

    Corrected Parser test answers

--- src/test/kotlin/br/com/moip/ParserTest.kt
+++ src/test/kotlin/br/com/moip/ParserTest.kt
@@ -9,11 +9,11 @@ import org.junit.Test
 
 abstract class ParserTest {
 
-    abstract fun getImplementation(): Parser?
+    abstract fun getImplementation(): Parser
 
     @Test
     fun `given an empty stream, when asked for first and status counts, then both should return empty`() {
-        val report = getImplementation()?.parse(javaClass.getResourceAsStream("/empty.txt"))!!
+        val report = getImplementation().parse(javaClass.getResourceAsStream("/empty.txt"))
 
         assertThat(report.getFirst(5), `is`(empty()))
         assertThat(report.getStatusCounts(), `is`(empty()))
@@ -21,31 +21,32 @@ abstract class ParserTest {
 
     @Test
     fun `given a sample stream, when asked for first three, then should return three more frequent urls`() {
-        val report = getImplementation()?.parse(javaClass.getResourceAsStream("/sample.txt"))!!
-
-        TODO("Discover real return of sample")
+        val report = getImplementation().parse(javaClass.getResourceAsStream("/sample.txt"))
 
         assertThat(report.getFirst(3), contains(
-            countOf("https://woodenoyster.com.br", 100),
-            countOf("https://grotesquemoon.de", 99),
-            countOf("https://notoriouslonesome.com", 90)
+            countOf("https://eagerhaystack.com", 750),
+            countOf("https://surrealostrich.com.br", 734),
+            countOf("https://grimpottery.net.br", 732)
         ))
     }
 
     @Test
     fun `given a sample stream, when asked for status count, then should return all status ordered`() {
-        val report = getImplementation()?.parse(javaClass.getResourceAsStream("/sample.txt"))!!
-
-        TODO("Discover real return of sample")
+        val report = getImplementation().parse(javaClass.getResourceAsStream("/sample.txt"))
 
         assertThat(report.getStatusCounts(), contains(
-            countOf("200", 100),
-            countOf("100", 99)
+            countOf("404", 1474),
+            countOf("503", 1451),
+            countOf("400", 1440),
+            countOf("500", 1428),
+            countOf("200", 1417),
+            countOf("201", 1402),
+            countOf("204", 1388)
         ))
     }
 
     private fun countOf(id: String, count: Int): Matcher<Count> =
         compose(
-            hasFeature(Count::id, `is`(id)),
-            hasFeature(Count::count, `is`(count)))
+            hasFeature("id", Count::id, `is`(id)),
+            hasFeature("count", Count::count, `is`(count)))
 }
